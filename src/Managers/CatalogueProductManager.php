aad17272624cd5a410ef3f75458782611985c057
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/CatalogueProductManager.php
+++ src/Managers/CatalogueProductManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\CatalogueProduct newEntity()
+ * @method \Amethyst\Schemas\CatalogueProductSchema getSchema()
+ * @method \Amethyst\Repositories\CatalogueProductRepository getRepository()
+ * @method \Amethyst\Serializers\CatalogueProductSerializer getSerializer()
+ * @method \Amethyst\Validators\CatalogueProductValidator getValidator()
+ * @method \Amethyst\Authorizers\CatalogueProductAuthorizer getAuthorizer()
+ */
 class CatalogueProductManager extends Manager
 {
     use ConfigurableManager;
