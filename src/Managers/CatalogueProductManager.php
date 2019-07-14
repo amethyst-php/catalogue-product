faffdf4587067607072ba3b57f5dcf1676f1440a
railken <railken@protonmail.ch>
2019-07-14 13:07:01 +0200

    fix style

--- src/Managers/CatalogueProductManager.php
+++ src/Managers/CatalogueProductManager.php
@@ -6,12 +6,12 @@ use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
 /**
- * @method \Amethyst\Models\CatalogueProduct newEntity()
- * @method \Amethyst\Schemas\CatalogueProductSchema getSchema()
+ * @method \Amethyst\Models\CatalogueProduct                 newEntity()
+ * @method \Amethyst\Schemas\CatalogueProductSchema          getSchema()
  * @method \Amethyst\Repositories\CatalogueProductRepository getRepository()
- * @method \Amethyst\Serializers\CatalogueProductSerializer getSerializer()
- * @method \Amethyst\Validators\CatalogueProductValidator getValidator()
- * @method \Amethyst\Authorizers\CatalogueProductAuthorizer getAuthorizer()
+ * @method \Amethyst\Serializers\CatalogueProductSerializer  getSerializer()
+ * @method \Amethyst\Validators\CatalogueProductValidator    getValidator()
+ * @method \Amethyst\Authorizers\CatalogueProductAuthorizer  getAuthorizer()
  */
 class CatalogueProductManager extends Manager
 {
